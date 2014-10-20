8ceefe3f6ae15501c55b2fb65b886753cd8e348a
t3hmrman <t3hmrman@gmail.com>
2014-10-20 00:42:09 -0500

    Added index template, stubs for CAS API methods, basic configuration struct and starting message

--- cas/v3/api.go
+++ cas/v3/api.go
@@ -10,6 +10,37 @@ import (
 )
 
 
+// Credential acceptor endpoint (requestor is handled in main)
 func HandleLogin(w http.ResponseWriter, r *http.Request) {
-	io.WriteString(w, "Login!")
+	// Show login page if credentials are not provided, attempt login otherwise
+	if (true) {
+		io.WriteString(w, "Login!")
+	} else {
+		io.WriteString(w, "Login!")		
+	}
+}
+
+// Endpoint for destroying CAS sessions (logging out) 
+func HandleLogout(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Endpoint for validating service tickets
+func HandleValidate(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Endpoint for validating service tickets (CAS 2.0)
+func HandleServiceValidate(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Endpoint for validating proxy tickets (CAS 2.0)
+func HandleProxyValidate(w http.ResponseWriter, r *http.Request) {
+
+}
+
+// Endpoint for handling proxy tickets (CAS 2.0)
+func HandleProxy(w http.ResponseWriter, r *http.Request) {
+
 }
--- main.go
+++ main.go
@@ -8,17 +8,30 @@ import (
 	"github.com/unrolled/render"
 )
 
+// Configuration object
+type CasServerConfig struct {
+	host string
+	port string
+}
+
 // Render object
 var r = render.New(render.Options{})
 
 func handleIndex(w http.ResponseWriter, req *http.Request) {
-	r.HTML(w, http.StatusOK, "login", map[string]string{"companyName": "CompanyABC"})
+	r.HTML(w, http.StatusOK, "index", map[string]string{"companyName": "CompanyABC"})
 }
 
 func main() {
+
+	// Configuration
+	config := &CasServerConfig{"0.0.0.0", ":8080"}
+
+	// Setup handlers
 	http.HandleFunc("/login", cas.HandleLogin)
 	http.HandleFunc("/", handleIndex)
-	err := http.ListenAndServe(":8080", nil)
+
+	log.Printf("Starting CasGo on port %s...\n", config.port)
+	err := http.ListenAndServe(config.port, nil)
 	if err != nil {
 		log.Fatal("Server Failed:", err)
 	}
