ebe062d5754ec6fe76d94875928f6f3b91a7ba0a
t3hmrman <t3hmrman@gmail.com>
2014-10-22 01:42:05 -0500

    Fixed context generation for template, added flash msg

--- cas/cas.go
+++ cas/cas.go
@@ -34,26 +34,29 @@ func New(config *CASServerConfig) *CAS {
 
 // (Optional) Handles Index route
 func (c *CAS) HandleIndex(w http.ResponseWriter, req *http.Request) {
-	c.render.HTML(w, http.StatusOK, "index", map[string]string{"companyName": "CompanyABC"})
+	c.render.HTML(w, http.StatusOK, "index", map[string]string{"CompanyName": c.config.CompanyName})
 }
 
 // Credential acceptor endpoint (requestor is Handled in main)
 func (c *CAS) HandleLogin(w http.ResponseWriter, req *http.Request) {
+	// Generate context
+	context := map[string]string{ "CompanyName": c.config.CompanyName }
+
 	// Show login page if credentials are not provided, attempt login otherwise
 	username := strings.ToLower(req.FormValue("email"))
 	password := strings.ToLower(req.FormValue("password"))
 	if username == "" || password == "" {
-		c.render.HTML(w, http.StatusOK, "login", nil)
+		c.render.HTML(w, http.StatusOK, "login", context)
 		return
-	}
-
-	// Attempt to log the user in
-	if username == "user@email.com" && password == "user" {
-		w.Write([]byte("Logged in!"))
 	} else {
-		w.Write([]byte("Failed to log in!"))
+		// Attempt to log the user in
+		if username == "user@email.com" && password == "user" {
+			w.Write([]byte("Logged in!"))
+		} else {
+			context["Error"] = "Invalid username/password combination"
+			c.render.HTML(w, http.StatusOK, "login", context)
+		}
 	}
-
 }
 
 // Endpoint for destroying CAS sessions (logging out)
