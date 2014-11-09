2972a91636fd420f3c4f1c7a4a069186fec6b037
Victor Adossi <t3hmrman@gmail.com>
2014-11-09 17:04:47 -0600

    Added register capability to login endpoint

--- cas/cas.go
+++ cas/cas.go
@@ -5,58 +5,62 @@ package cas
  */
 
 import (
-	"github.com/unrolled/render"
-	"net/http"
-	"strings"
+    "github.com/unrolled/render"
+    "net/http"
+    "strings"
 )
 
 // CAS server interface
 type CASServer interface {
-	HandleLogin(w http.ResponseWriter, r *http.Request)
-	HandleLogout(w http.ResponseWriter, r *http.Request)
-	HandleValidate(w http.ResponseWriter, r *http.Request)
-	HandleServiceValidate(w http.ResponseWriter, r *http.Request)
-	HandleProxyValidate(w http.ResponseWriter, r *http.Request)
-	HandleProxy(w http.ResponseWriter, r *http.Request)
+    HandleLogin(w http.ResponseWriter, r *http.Request)
+    HandleLogout(w http.ResponseWriter, r *http.Request)
+    HandleRegister(w http.ResponseWriter, r *http.Request)
+    HandleValidate(w http.ResponseWriter, r *http.Request)
+    HandleServiceValidate(w http.ResponseWriter, r *http.Request)
+    HandleProxyValidate(w http.ResponseWriter, r *http.Request)
+    HandleProxy(w http.ResponseWriter, r *http.Request)
 }
 
 // CAS Server
 type CAS struct {
-	config *CASServerConfig
-	render *render.Render
+    config *CASServerConfig
+    render *render.Render
 }
 
 func New(config *CASServerConfig) *CAS {
-	r := render.New(render.Options{Directory: config.TemplatesDirectory})
-	c := &CAS{config, r}
-	return c
+    r := render.New(render.Options{Directory: config.TemplatesDirectory})
+    c := &CAS{config, r}
+    return c
 }
 
 // (Optional) Handles Index route
 func (c *CAS) HandleIndex(w http.ResponseWriter, req *http.Request) {
-	c.render.HTML(w, http.StatusOK, "index", map[string]string{"CompanyName": c.config.CompanyName})
+    c.render.HTML(w, http.StatusOK, "index", map[string]string{"CompanyName": c.config.CompanyName})
 }
 
 // Credential acceptor endpoint (requestor is Handled in main)
 func (c *CAS) HandleLogin(w http.ResponseWriter, req *http.Request) {
-	// Generate context
-	context := map[string]string{ "CompanyName": c.config.CompanyName }
+    // Generate context
+    context := map[string]string{
+        "CompanyName": c.config.CompanyName,
+        "Register": req.FormValue("register"),
+    }
 
-	// Show login page if credentials are not provided, attempt login otherwise
-	username := strings.ToLower(req.FormValue("email"))
-	password := strings.ToLower(req.FormValue("password"))
-	if username == "" || password == "" {
-		c.render.HTML(w, http.StatusOK, "login", context)
-		return
-	} else {
-		// Attempt to log the user in
-		if username == "user@email.com" && password == "user" {
-			w.Write([]byte("Logged in!"))
-		} else {
-			context["Error"] = "Invalid username/password combination"
-			c.render.HTML(w, http.StatusOK, "login", context)
-		}
-	}
+    // Show login page if credentials are not provided, attempt login otherwise
+    username := strings.ToLower(req.FormValue("email"))
+    password := strings.ToLower(req.FormValue("password"))
+    if username == "" || password == "" {
+        c.render.HTML(w, http.StatusOK, "login", context)
+        return
+    } else {
+        // Attempt to log the user in
+        if username == "user@email.com" && password == "user" {
+            w.Write([]byte("Logged in!"))
+        } else {
+            context["Error"] = "Invalid username/password combination"
+            c.render.HTML(w, http.StatusOK, "login", context)
+        }
+    }
 }
 
 // Endpoint for destroying CAS sessions (logging out)
