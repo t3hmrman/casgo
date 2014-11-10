8438ada61d5bcfffff1475b3c2e1b90ff94587bf
Victor Adossi <t3hmrman@gmail.com>
2014-11-09 18:05:36 -0600

    Update styling on login/register pages, backend for register working

--- cas/cas.go
+++ cas/cas.go
@@ -5,99 +5,105 @@ package cas
  */
 
 import (
-	"code.google.com/p/go.crypto/bcrypt"
-	r "github.com/dancannon/gorethink"
-	"github.com/unrolled/render"
-	"net/http"
-	"strings"
+    "code.google.com/p/go.crypto/bcrypt"
+    r "github.com/dancannon/gorethink"
+    "github.com/unrolled/render"
+    "net/http"
+    "strings"
 )
 
 // CAS server interface
 type CASServer interface {
-	HandleLogin(w http.ResponseWriter, r *http.Request)
-	HandleLogout(w http.ResponseWriter, r *http.Request)
-	HandleRegister(w http.ResponseWriter, r *http.Request)
-	HandleValidate(w http.ResponseWriter, r *http.Request)
-	HandleServiceValidate(w http.ResponseWriter, r *http.Request)
-	HandleProxyValidate(w http.ResponseWriter, r *http.Request)
-	HandleProxy(w http.ResponseWriter, r *http.Request)
+    HandleLogin(w http.ResponseWriter, r *http.Request)
+    HandleLogout(w http.ResponseWriter, r *http.Request)
+    HandleRegister(w http.ResponseWriter, r *http.Request)
+    HandleValidate(w http.ResponseWriter, r *http.Request)
+    HandleServiceValidate(w http.ResponseWriter, r *http.Request)
+    HandleProxyValidate(w http.ResponseWriter, r *http.Request)
+    HandleProxy(w http.ResponseWriter, r *http.Request)
 }
 
 // CAS Server
 type CAS struct {
-	config *CASServerConfig
-	render *render.Render
+    config *CASServerConfig
+    render *render.Render
 }
 
 func New(config *CASServerConfig) *CAS {
-	r := render.New(render.Options{Directory: config.TemplatesDirectory})
-	c := &CAS{config, r}
-	return c
+    r := render.New(render.Options{Directory: config.TemplatesDirectory})
+    c := &CAS{config, r}
+    return c
 }
 
 // (Optional) Handles Index route
 func (c *CAS) HandleIndex(w http.ResponseWriter, req *http.Request) {
-	c.render.HTML(w, http.StatusOK, "index", map[string]string{"CompanyName": c.config.CompanyName})
+    c.render.HTML(w, http.StatusOK, "index", map[string]string{"CompanyName": c.config.CompanyName})
 }
 
 // Credential acceptor endpoint (requestor is Handled in main)
 func (c *CAS) HandleLogin(w http.ResponseWriter, req *http.Request) {
-	// Generate context
-	context := map[string]string{
-		"CompanyName": c.config.CompanyName,
-	}
+    // Generate context
+    context := map[string]string{
+        "CompanyName": c.config.CompanyName,
+    }
 
-	// Show login page if credentials are not provided, attempt login otherwise
-	email := strings.TrimSpace(strings.ToLower(req.FormValue("email")))
-	password := strings.TrimSpace(strings.ToLower(req.FormValue("password")))
+    // Show login page if credentials are not provided, attempt login otherwise
+    email := strings.TrimSpace(strings.ToLower(req.FormValue("email")))
+    password := strings.TrimSpace(strings.ToLower(req.FormValue("password")))
 
-	// Exit early if email/password are empty
-	if email == "" || password == "" {
-		c.render.HTML(w, http.StatusOK, "login", context)
-		return
-	}
+    // Exit early if email/password are empty
+    if email == "" || password == "" {
+        c.render.HTML(w, http.StatusOK, "login", context)
+        return
+    }
 
-	// Attempt to log the user in
-	if email == "user@email.com" && password == "user" {
-		w.Write([]byte("Logged in!"))
-	} else {
-		context["Error"] = "Invalid email/password combination"
-		c.render.HTML(w, http.StatusOK, "login", context)
-	}
+    // Attempt to log the user in
+    if email == "user@email.com" && password == "user" {
+        w.Write([]byte("Logged in!"))
+    } else {
+        context["Error"] = "Invalid email/password combination"
+        c.render.HTML(w, http.StatusOK, "login", context)
+    }
 
 }
 
 // Endpoint for destroying CAS sessions (logging out)
 func (c *CAS) HandleRegister(w http.ResponseWriter, req *http.Request) {
-	context := map[string]string{
-		"CompanyName": c.config.CompanyName,
-		"Registered": "",
-	}
+    context := map[string]string{
+        "CompanyName": c.config.CompanyName,
+    }
 
-	// Show login page if credentials are not provided, attempt login otherwise
-	email := strings.TrimSpace(strings.ToLower(req.FormValue("email")))
-	password := strings.TrimSpace(strings.ToLower(req.FormValue("password")))
+    // Show login page if credentials are not provided, attempt login otherwise
+    email := strings.TrimSpace(strings.ToLower(req.FormValue("email")))
+    password := strings.TrimSpace(strings.ToLower(req.FormValue("password")))
 
-	encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10) // Default cost
-	if err != nil {
-		context["Error"] = "Registration failed... Please contact server administrator"
-		c.render.HTML(w, http.StatusInternalServerError, "register", context)
-	}
-	
-	// Create new user object
-	newUser := map[string]string{
-		"email": email,
-		"password": string(encryptedPassword),
-	}
+    // Exit early if email/password are empty
+    if email == "" || password == "" {
+        c.render.HTML(w, http.StatusOK, "register", context)
+        return
+    }
 
-	_, err = r.Db(c.config.DBName).Table("users").Insert(newUser).Run(c.config.RDBSession)
-	if err != nil {
-		c.render.HTML(w, http.StatusOK, "register", context)
-		return
-	}
+    encryptedPassword, err := bcrypt.GenerateFromPassword([]byte(password), 10) // Default cost
+    if err != nil {
+        context["Error"] = "Registration failed... Please contact server administrator"
+        c.render.HTML(w, http.StatusInternalServerError, "register", context)
+        return
+    }
 
-	context["Registered"] = "true"
-	c.render.HTML(w, http.StatusOK, "register", context)
+    // Create new user object
+    newUser := map[string]string{
+        "email": email,
+        "password": string(encryptedPassword),
+    }
+
+    _, err = r.Db(c.config.DBName).Table("users").Insert(newUser).RunWrite(c.config.RDBSession)
+    if err != nil {
+        c.render.HTML(w, http.StatusOK, "register", context)
+        return
+    }
+
+    context["Success"] = "Successfully registered email and password, Please use it to <a href=\"/login\">login</a>."
+    c.render.HTML(w, http.StatusOK, "register", context)
 }
 
 // Endpoint for destroying CAS sessions (logging out)
