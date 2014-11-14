e0cf8a9f5718cf1dd1cbbbe129d4bf6c88220dd7
Victor Adossi <t3hmrman@gmail.com>
2014-11-13 22:26:59 -0600

    Added skeleton of working integration test

--- cas/cas_test.go
+++ cas/cas_test.go
@@ -1,9 +1,37 @@
 package cas
 
-import "testing"
+import (
+	"golang.org/x/net/html"
+	"testing"
+	"net/http/httptest"
+	"net/http"
+	"log"
+	"fmt"
+)
 
 func TestLoginEndpoint(t *testing.T){
 	if (true == false) {
 		t.Error("useless test")
+	}
+}
+
+// Login page tests
+func TestLoginPage(t *testing.T) {
+	if testing.Short() { t.Skip("skipping test in short mode.") }
+
+	testHandler := http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {		fmt.Fprintln(w, "Hello, client")
+	})
+	
+	loginServer := httptest.NewServer(testHandler)
+	defer loginServer.Close()
+
+	res, err := http.Get(loginServer.URL)
+	if err != nil { log.Fatal(err) }
+
+	doc, err := html.Parse(res.Body)
+	if err != nil { log.Fatal(err) }
+
+	if doc == nil {
+		t.Error("Doc is nil!")
 	}
 }
