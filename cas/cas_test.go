4eb3573a3811aaf080b2649148eda7db97c9dbae
t3hmrman <t3hmrman@gmail.com>
2014-11-23 22:52:40 -0600

    Add more CAS Server tests

--- cas/cas_test.go
+++ cas/cas_test.go
@@ -1,6 +1,7 @@
 package cas
 
 import (
+	//r "github.com/dancannon/gorethink"
 	"golang.org/x/net/html"
 	"testing"
 	"net/http/httptest"
@@ -9,9 +10,41 @@ import (
 	"fmt"
 )
 
-func TestLoginEndpoint(t *testing.T){
-	if (true == false) {
-		t.Error("useless test")
+// CAS Server creation should fail if no configuration is provided
+func TestNilConfigCASServerCreation(t *testing.T) {
+	server, err := NewCASServer(nil)
+	if server != nil || err == nil {
+		t.Error("CAS Server should have failed with nil configuration object")
+	}
+}
+
+// CAS Server creation should succeed if default configuration is made
+func TestDefaultConfigCASServerCreation(t *testing.T) {
+	config, err := NewCASServerConfig(nil)
+	if err != nil { t.Error("Error creating config:", err) }
+
+	server, err := NewCASServer(config)
+	if server == nil || err != nil { t.Error("Server creation failed:", err) }
+}
+
+func setupCASServer(t *testing.T) *CAS {
+	config, err := NewCASServerConfig(nil)
+	if err != nil { t.Error("Error creating config:", err) }
+
+	server, err := NewCASServer(config)
+	if server == nil || err != nil { t.Error("Server creation failed:", err) }
+
+	return server
+}
+
+// CAS Server init should properly attach handler functions to expected addresses
+func TestCASGetAddrFn(t *testing.T) {
+	server := setupCASServer(t)
+	expectedAddress := CONFIG_DEFAULTS["host"] + ":" + CONFIG_DEFAULTS["port"]
+	actualAddress := server.GetAddr()
+
+	if actualAddress != expectedAddress {
+		t.Error("Expected address [%s], got [%s]", expectedAddress, actualAddress)
 	}
 }
 
@@ -21,7 +54,7 @@ func TestLoginPage(t *testing.T) {
 
 	testHandler := http.HandlerFunc(func (w http.ResponseWriter, r *http.Request) {		fmt.Fprintln(w, "Hello, client")
 	})
-	
+
 	loginServer := httptest.NewServer(testHandler)
 	defer loginServer.Close()
 
