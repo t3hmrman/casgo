8ddb289915a103992ce0e48062d66c8c1351fc80
t3hmrman <t3hmrman@gmail.com>
2014-12-06 21:19:50 -0600

    Modify db tests to use testing harness, only setup/teardown tables, not whole DB

--- cas/db_test.go
+++ cas/db_test.go
@@ -53,7 +53,7 @@ func TestDbExists(t *testing.T) {
 		}
 		t.Errorf("Failed to check if database exists, err:", casErr)
 	}
-	
+
 	if !exists {
 		t.Error("Database [%s] should have been set up by setupDb, but does not exist", s.dbAdapter.getDbName())
 	}
@@ -128,7 +128,7 @@ func dbTestHarness(t *testing.T, fixturesToLoad []StringTuple, dbTestFunc func(*
 
 	// Setup CAS Server & DB modified for test
 	s := setupTestCASServer(t)
-	
+
 	// Setup the test database if it doesn't already exists
 	exists, err := s.dbAdapter.DbExists()
 	if err != nil {
@@ -141,7 +141,7 @@ func dbTestHarness(t *testing.T, fixturesToLoad []StringTuple, dbTestFunc func(*
 		setupDb(s, t)
 	}
 
-	// Possibly load fixtures
+	// Setup tables and load fixtures
 	for _, fixtureTuple := range fixturesToLoad {
 		tableName, fixturePath := fixtureTuple.First(), fixtureTuple.Second()
 
@@ -153,14 +153,17 @@ func dbTestHarness(t *testing.T, fixturesToLoad []StringTuple, dbTestFunc func(*
 		}
 
 		// Load fixture into table
-		casErr := s.dbAdapter.LoadJSONFixture(s.dbAdapter.getDbName(), tableName, fixturePath)
-		if casErr != nil {
-			if casErr.err != nil {
-				t.Logf("DB error: %s", casErr.msg)
-				t.Errorf("Failed to import data into database: %s", *casErr.err)
-				return
+		if fixturePath != "" {
+			casErr := s.dbAdapter.LoadJSONFixture(s.dbAdapter.getDbName(), tableName, fixturePath)
+			if casErr != nil {
+				if casErr.err != nil {
+					t.Logf("DB error: %s", casErr.msg)
+					t.Errorf("Failed to import data into database: %s", *casErr.err)
+					return
+				}
 			}
 		}
+
 	}
 
 	// Run test function
@@ -239,54 +242,35 @@ func TestFindUserByEmail(t *testing.T) {
 
 // Test adding new user
 func TestAddNewUser(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping DB-involved test (in short mode).")
+	fixturesToLoad := []StringTuple{
+		StringTuple{"users", ""},
 	}
+	dbTestHarness(t, fixturesToLoad, func(t *testing.T, s *CAS) {
 
-	s := setupTestCASServer(t)
-	setupDb(s, t)
-
-	// Setup users table
-	casErr := s.dbAdapter.SetupUsersTable()
-	if casErr != nil {
-		if casErr.err != nil {
-			t.Logf("DB Error: %v", *casErr.err)
+		// Add the user
+		newUser, casErr := s.dbAdapter.AddNewUser("test_user@test.com", "randompassword")
+		if casErr != nil {
+			t.Errorf("Failed to add new user, err: %v", casErr)
+			return
 		}
-		t.Errorf("Failed to setup users table, err: %v", casErr)
-		return
-	}
 
-	// Add the user
-	newUser, casErr := s.dbAdapter.AddNewUser("test_user@test.com", "randompassword")
-	if casErr != nil {
-		t.Errorf("Failed to add new user, err: %v", casErr)
-		return
-	}
+		// Find the user by email
+		returnedUser, casErr := s.dbAdapter.FindUserByEmail(newUser.Email)
+		if casErr != nil {
+			t.Errorf("Failed to find created temporary user, err: %v", casErr)
+			return
+		}
 
-	// Find the user by email
-	returnedUser, casErr := s.dbAdapter.FindUserByEmail(newUser.Email)
-	if casErr != nil {
-		t.Errorf("Failed to find created temporary user, err: %v", casErr)
-		return
-	}
+		if returnedUser != nil && newUser.Email != returnedUser.Email {
+			t.Errorf("Newly created user and returned user's emails don't match")
+			return
+		}
 
-	if returnedUser != nil && newUser.Email != returnedUser.Email {
-		t.Errorf("Newly created user and returned user's emails don't match")
-		return
-	}
-
-	teardownDb(s, t)
+	})
 }
 
 // Utility function for creating a service and adding a ticket to it
 func addTicketForService(s *CAS, t *testing.T) (*CASTicket, *CASService, *CASServerError) {
-
-	// Setup tickets table
-	casErr := s.dbAdapter.SetupTicketsTable()
-	if casErr != nil {
-		t.Logf("Failed to setup tickets table")
-		return nil, nil, casErr
-	}
 
 	// Create a new CASTicket to store
 	ticket := &CASTicket{
@@ -301,7 +285,7 @@ func addTicketForService(s *CAS, t *testing.T) (*CASTicket, *CASService, *CASSer
 		AdminEmail: "noone@nowhere.com",
 	}
 
-	ticket, casErr = s.dbAdapter.AddTicketForService(ticket, mockService)
+	ticket, casErr := s.dbAdapter.AddTicketForService(ticket, mockService)
 	if casErr != nil {
 		t.Logf("Failed to add ticket to database for service [%s]", mockService.Name)
 		return nil, nil, casErr
@@ -318,96 +302,81 @@ func addTicketForService(s *CAS, t *testing.T) (*CASTicket, *CASService, *CASSer
 
 // Test adding ticket for service
 func TestAddTicketForService(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping DB-involved test (in short mode).")
+	fixturesToLoad := []StringTuple{
+		StringTuple{"tickets", ""},
 	}
+	dbTestHarness(t, fixturesToLoad, func(t *testing.T, s *CAS) {
 
-	// Setup server and Db
-	s := setupTestCASServer(t)
-	setupDb(s, t)
+		// Add ticket for the service
+		addTicketForService(s, t)
 
-	// Add ticket for the service
-	addTicketForService(s, t)
-
-	teardownDb(s, t)
+	})
 }
 
 // Test finding tickets by Id given a service
 func TestFindTicketByIdForService(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping DB-involved test (in short mode).")
+	fixturesToLoad := []StringTuple{
+		StringTuple{"tickets", ""},
 	}
+	dbTestHarness(t, fixturesToLoad, func(t *testing.T, s *CAS) {
 
-	// Setup server and Db
-	s := setupTestCASServer(t)
-	setupDb(s, t)
-
-	// Add ticket for the service
-	originalTicket, service, casErr := addTicketForService(s, t)
-	if casErr != nil {
-		if casErr.err != nil {
-			t.Logf("DB err: %v", *casErr.err)
+		// Add ticket for the service
+		originalTicket, service, casErr := addTicketForService(s, t)
+		if casErr != nil {
+			if casErr.err != nil {
+				t.Logf("DB err: %v", *casErr.err)
+			}
+			t.Errorf("Utility function to add ticket for service failed, err: %v", casErr)
+			return
 		}
-		t.Errorf("Utility function to add ticket for service failed, err: %v", casErr)
-		return
-	}
 
-	// Find the ticket that was just added
-	ticket, err := s.dbAdapter.FindTicketByIdForService(originalTicket.Id, service)
-	if err != nil {
-		t.Errorf("Failed to find ticket that should have been added: %v", originalTicket)
-		return
-	}
+		// Find the ticket that was just added
+		ticket, err := s.dbAdapter.FindTicketByIdForService(originalTicket.Id, service)
+		if err != nil {
+			t.Errorf("Failed to find ticket that should have been added: %v", originalTicket)
+			return
+		}
 
-	// Ensure the tickets are the same
-	if ticket != nil && originalTicket != nil && !compareTickets(*ticket, *originalTicket) {
-		t.Errorf("Found ticket ( %v ) != original ticket ( %v )", ticket, originalTicket)
-		return
-	}
+		// Ensure the tickets are the same
+		if ticket != nil && originalTicket != nil && !compareTickets(*ticket, *originalTicket) {
+			t.Errorf("Found ticket ( %v ) != original ticket ( %v )", ticket, originalTicket)
+			return
+		}
 
-	teardownDb(s, t)
+	})
 }
 
 // Test removing an added tickets for a given user
 func TestRemoveTicketsForUser(t *testing.T) {
-	if testing.Short() {
-		t.Skip("Skipping DB-involved test (in short mode).")
+	fixturesToLoad := []StringTuple{
+		StringTuple{"tickets", ""},
+		StringTuple{"users", ""},
 	}
+	dbTestHarness(t, fixturesToLoad, func(t *testing.T, s *CAS) {
 
-	// Setup server and DB
-	s := setupTestCASServer(t)
-	casErr := setupDb(s, t)
-	if casErr != nil {
-		if casErr.err != nil {
-			t.Logf("DB err: %v", *casErr.err)
+		// Add a ticket for the user
+		ticket, service, casErr := addTicketForService(s, t)
+		if casErr != nil {
+			if casErr.err != nil {
+				t.Logf("DB err: %v", *casErr.err)
+			}
+			t.Errorf("Utility function to add ticket for service failed, err: %v", casErr)
+			return
 		}
-		t.Errorf("Utility function to add ticket for service failed, err: %v", casErr)
-		return
-	}
 
-	// Add a ticket for the user
-	ticket, service, casErr := addTicketForService(s, t)
-	if casErr != nil {
-		if casErr.err != nil {
-			t.Logf("DB err: %v", *casErr.err)
+		// Remove ticket for the user
+		err := s.dbAdapter.RemoveTicketsForUserWithService(ticket.UserEmail, service)
+		if err != nil {
+			t.Errorf("Failed to remove tickets for user with service, err: %v", casErr)
+			return
 		}
-		t.Errorf("Utility function to add ticket for service failed, err: %v", casErr)
-		return
-	}
 
-	// Remove ticket for the user
-	err := s.dbAdapter.RemoveTicketsForUserWithService(ticket.UserEmail, service)
-	if err != nil {
-		t.Errorf("Failed to remove tickets for user with service, err: %v", casErr)
-		return
-	}
+		// Attempt to find ticket (that should have been removed
+		ticket, err = s.dbAdapter.FindTicketByIdForService(ticket.Id, service)
+		if ticket != nil || err == nil {
+			t.Errorf("Found ticket (or did not recieve expected error) that should have been deleted: %v", ticket)
+			return
+		}
 
-	// Attempt to find ticket (that should have been removed
-	ticket, err = s.dbAdapter.FindTicketByIdForService(ticket.Id, service)
-	if ticket != nil || err == nil {
-		t.Errorf("Found ticket (or did not recieve expected error) that should have been deleted: %v", ticket)
-		return
-	}
-
-	teardownDb(s, t)
+	})
 }
