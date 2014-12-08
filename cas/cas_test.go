42a4741d9afdc59f9382fb1ac8e2c96fa7b0d9c5
t3hmrman <t3hmrman@gmail.com>
2014-12-08 00:11:02 -0600

    Start HTTP tests porting

--- cas/cas_test.go
+++ cas/cas_test.go
@@ -2,7 +2,9 @@ package cas_test
 
 import (
 	. "github.com/t3hmrman/casgo/cas"
+	"net/http/httptest"
 
+	"github.com/PuerkitoBio/goquery"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
 )
@@ -29,11 +31,7 @@ var _ = Describe("Cas", func() {
 		})
 
 		It("Should produce a predicatable address from the GetAddr function", func() {
-			config, err := NewCASServerConfig(map[string]string{
-				"companyName":        "Casgo Testing Company",
-				"dbName":             "casgo_test",
-				"templatesDirectory": "../templates",
-			})
+			config, err := NewCASServerConfig(nil)
 			Expect(err).To(BeNil())
 
 			server, err := NewCASServer(config)
@@ -44,7 +42,32 @@ var _ = Describe("Cas", func() {
 			actualAddress := server.GetAddr()
 			Expect(actualAddress).To(Equal(expectedAddress))
 		})
-
 	})
 
+	Describe("CAS Website", func() {
+		// Setup CAS server
+		config, _ := NewCASServerConfig(map[string]string{
+			"companyName":        "Casgo Testing Company",
+			"dbName":             "casgo_test",
+			"templatesDirectory": "../templates",
+		})
+		server, _ := NewCASServer(config)
+		server.SetupDb()
+		defer server.TeardownDb()
+
+		// Setup http test server
+		httpTestServer := httptest.NewServer(server.ServeMux)
+		defer httpTestServer.Close()
+
+		It("Should have an working index page", func() {
+			// Visit index endpoint
+			doc, err := goquery.NewDocument(httpTestServer.URL)
+			Expect(err).To(BeNil())
+
+			// Ensure title of index page (endpoint "") contains what we expect
+			expectedText, actualText := "CasGo", doc.Find("title").Text()
+			Expect(actualText).To(Equal(expectedText))
+		})
+
+	})
 })
