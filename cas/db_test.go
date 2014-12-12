371c975e762f1d67cc38ebd8d9bc78d0d752de1a
t3hmrman <t3hmrman@gmail.com>
2014-12-11 23:11:18 -0600

    Ported getters tests

--- cas/db_test.go
+++ cas/db_test.go
@@ -8,13 +8,41 @@ import (
 	. "github.com/onsi/gomega"
 )
 
-var _ = Describe("Cas DB", func() {
+var _ = Describe("Cas DB adapter", func() {
 
-	Describe("DbExists", func() {
+	Describe("DbExists function", func() {
 		It("should return whether the database exists or not", func() {
 			exists, casErr := testCASServer.Db.DbExists()
 			Expect(casErr).To(BeNil())
 			Expect(exists).To(Equal(true))
+		})
+	})
+
+	Describe("GetDbName function", func() {
+		It("should return the name of the server", func() {
+			actual, expected := testCASServer.Db.GetDbName(), testCASServer.Config["dbName"]
+			Expect(actual).To(Equal(expected))
+		})
+	})
+
+	Describe("GetUsersTableName function", func() {
+		It("should return the default if not set differently", func() {
+			actual, expected := testCASServer.Db.GetUsersTableName(), "users"
+			Expect(actual).To(Equal(expected))
+		})
+	})
+
+	Describe("GetServicesTableName function", func() {
+		It("should return the default if not set differently", func() {
+			actual, expected := testCASServer.Db.GetServicesTableName(), "services"
+			Expect(actual).To(Equal(expected))
+		})
+	})
+
+	Describe("GetTicketsTableName function", func() {
+		It("should return the default if not set differently", func() {
+			actual, expected := testCASServer.Db.GetTicketsTableName(), "tickets"
+			Expect(actual).To(Equal(expected))
 		})
 	})
 
