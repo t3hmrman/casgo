dbf21b48518e2b1254d6577ad919b2e25af95693
t3hmrman <t3hmrman@gmail.com>
2014-12-16 01:09:51 -0600

    Fixed director routes and setup intial VM

--- public/js/casgo.js
+++ public/js/casgo.js
@@ -1,12 +1,25 @@
 /* global ko */
 
+// Create App namespace on global scope
+window.App = {
+  VM: null,
+  Routes: null
+};
+
 /**
  * View model for top-level casgo app
  *
  * @exports CasgoViewModel
  */
 function CasgoViewModel() {
+  var self = this;
 
+  // Routes
+  self.ServicesRoute = function() { console.log("Services route!"); };
+  self.ManageRoute = function() { console.log("Manage route!"); };
+  self.ManageUsersRoute = function() { console.log("Manage users route!"); };
+  self.ManageServicesRoute = function() { console.log("Manage services route!"); };
+  self.StatisticsRoute = function() { console.log("Statistics route!"); };
 }
 
 // Attach VM
--- public/js/routes.js
+++ public/js/routes.js
@@ -1,13 +1,14 @@
-// Create App namespace on global scope
-window.App = {
-  VM: null,
-  Routes: null
-};
+/* global Router */
 
 // Routes for App
 window.App.Routes = {
-  '/': function() {
-    console.log("Home route");
-  }
+  '/services': window.App.VM.ServicesRoute,
+  '/manage': window.App.VM.ManageRoute,
+  '/manage/users': window.App.VM.ManageUsersRoute,
+  '/manage/services': window.App.VM.ManageServicesRoute,
+  '/statistics': window.App.VM.StatisticsRoute
 };
-  
+
+// Create and initialize Director's router
+window.App.Router = Router(window.App.Routes);
+window.App.Router.init();
