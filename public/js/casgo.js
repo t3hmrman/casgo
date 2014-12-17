cbf980ca29fe23eb7fd231f16f88fecb9aad1a2d
t3hmrman <t3hmrman@gmail.com>
2014-12-16 23:02:45 -0600

    Add working routing and simple templating system

--- public/js/casgo.js
+++ public/js/casgo.js
@@ -1,4 +1,5 @@
 /* global ko */
+'use strict';
 
 // Create App namespace on global scope
 window.App = {
@@ -15,11 +16,37 @@ function CasgoViewModel() {
   var self = this;
 
   // Routes
-  self.ServicesRoute = function() { console.log("Services route!"); };
   self.ManageRoute = function() { console.log("Manage route!"); };
   self.ManageUsersRoute = function() { console.log("Manage users route!"); };
   self.ManageServicesRoute = function() { console.log("Manage services route!"); };
   self.StatisticsRoute = function() { console.log("Statistics route!"); };
+
+  // Routing utility functions
+  self.currentRoute =  ko.observable(window.location.hash.slice(1));
+  self.currentRouteIs = function(route) {return self.currentRoute() === route; };
+  self.gotoRoute =  function(route) {
+    console.log("Going to route:", route);
+    window.location.href = '#' + route;
+    self.currentRoute(window.location.hash.slice(1));
+  };
+
+
+  // Controllers
+  self.ServicesCtrl = {
+    setup: function() {
+      console.log("Would have loaded services!");
+    },
+    test: function() { console.log("TEST!"); },
+    services: ko.observableArray([])
+  };
+
+  self.ManageCtrl = {};
+  self.ManageUsersCtrl = {users: []};
+  self.ManageServicesCtrl = {services: []};
+  self.StatisticsCtrl = {};
+
+
+
 }
 
 // Attach VM
