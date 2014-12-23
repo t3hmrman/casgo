8fad5c712f3b1bec4630e9b822848d38acea03f2
t3hmrman <t3hmrman@gmail.com>
2014-12-22 23:45:05 -0600

    Add complexity/abstraction to in-app routing, handle nested routes by simple prefix checking

--- public/js/casgo.js
+++ public/js/casgo.js
@@ -1,12 +1,13 @@
 /* global ko */
 /* global fetch */
 /* global q */
+/* global _ */
 'use strict';
 
 // Create App namespace on global scope
 window.App = {
-	VM: null,
-	Routes: null
+  VM: null,
+  Routes: {}
 };
 
 /**
@@ -15,182 +16,214 @@ window.App = {
  * @exports CasgoViewModel
  */
 function CasgoViewModel() {
-	var vm = this;
+  var vm = this;
 
-	/**
-	 * Routing utility functions
-	 */
-	vm.currentRoute =  ko.observable(window.location.hash.slice(1));
-	vm.currentRouteIs = function(route) {return vm.currentRoute() === route; };
-	vm.gotoRoute =  function(route) {
-		window.location.href = '#' + route;
-		vm.currentRoute(window.location.hash.slice(1));
+  vm.currentRouteUrl =  ko.observable(window.location.hash.slice(1));
+  /**
+   * Routing utility functions
+   */
 
-		// Run setup functions for all specified controllers, if any
-		if (_.has(window.App.Routes, route) &&
-				_.has(window.App.Routes[route], 'controllers') &&
-				_.isArray(window.App.Routes[route].controllers)) {
+  /**
+   * Check if the current {@link currentRouteUrl} against a given URL
+   *
+   * @param {string} url - The URL to check
+   * @returns whether the current route url is exactly the given value
+   */
+  vm.currentRouteUrlIs = function(url) {return vm.currentRouteUrl() === url; };
 
-			_.forEach(window.App.Routes[route].controllers, function(c) {
-				if (_.isString(c) &&
-						_.has(vm, c) &&
-						_.has(vm[c], 'setup') &&
-						_.isFunction(vm[c].setup)) {
-					vm[c].setup();
-					// TODO: Pass route args
-				}
+  /**
+   * Check whether the current route URL starts with a given URL
+   * This usually indicates that it is a sub-route
+   *
+   * @param {string} prefix - The prefix to search for 
+   * @returns whether the current route URL contains the prefix
+   */
+  vm.currentRouteUrlHasPrefix = function(prefix) { return vm.currentRouteUrl().match('^' + prefix); };
 
-			});
-		}
-	};
+  /**
+   * Lookup route by it's url
+   *
+   * @param {string} url - The URL to use to look up the route
+   * @returns the first route that matche the URL exactly
+   */
+  vm.getRouteByUrl = function(url) {
+    return _.first(_.values(window.App.Routes), function(v) { return v.url === url; });
+  };
+
+  /**
+   * Navigate to a route
+   * 
+   * @param {object} route - The route to navigate to
+   */
+  vm.gotoRoute =  function(route) {
+    window.location.href = '#' + route.url;
+    vm.currentRouteUrl(window.location.hash.slice(1));
+
+    // Run setup functions for all specified controllers, if any
+    if (_.has(window.App.Routes, route) &&
+        _.has(window.App.Routes[route], 'controllers') &&
+        _.isArray(window.App.Routes[route].controllers)) {
+
+      _.forEach(window.App.Routes[route].controllers, function(c) {
+        if (_.isString(c) &&
+            _.has(vm, c) &&
+            _.has(vm[c], 'setup') &&
+            _.isFunction(vm[c].setup)) {
+          vm[c].setup();
+          // TODO: Pass route args
+        }
+
+      });
+    }
+  };
 
 
-	/**
-	 * Service that maintains user sessions and information retrieved from the backend
-	 */
-	vm.SessionService = {
-		currentUser: ko.observable({}),
+  /**
+   * Service that maintains user sessions and information retrieved from the backend
+   */
+  vm.SessionService = {
+    currentUser: ko.observable({}),
 
-		/**
-		 * Get the current user
-		 *
-		 * @returns A Promise that will resolve to the user object
-		 */
-		getCurrentUser: function() {
-			var svc = vm.SessionService;
-			return new Promise(function(resolve, reject) {
-				if (_.isEmpty(svc.currentUser())) {
-					svc
-						.fetchCurrentUser()
-						.then(resolve);
-				} else {
-					resolve(svc.currentUser());
-				}
-			});
-		},
+    /**
+     * Get the current user
+     *
+     * @returns A Promise that will resolve to the user object
+     */
+    getCurrentUser: function() {
+      var svc = vm.SessionService;
+      return new Promise(function(resolve, reject) {
+        if (_.isEmpty(svc.currentUser())) {
+          svc
+            .fetchCurrentUser()
+            .then(resolve);
+        } else {
+          resolve(svc.currentUser());
+        }
+      });
+    },
 
-		/**
-		 * Fetch the current user in the session from the backend, updating the service
-		 *
-		 * @returns a promise that will resolve to the user object
-		 */
-		fetchCurrentUser: function() {
-			var svc = vm.SessionService;
-			return new Promise(function(resolve, reject) {
-				fetch('/api/sessions')
-					.then(function(resp){
-						return resp.json();
-					}).then(function(json) {
-						if (json.status === "success") {
-							svc.currentUser(json.data);
-							resolve(svc.currentUser());
-						} else {
-							reject(new Error("API call failed", json.message));
-						}
-				}).catch(function(err) {
-					reject(err);
-				});
+    /**
+     * Fetch the current user in the session from the backend, updating the service
+     *
+     * @returns a promise that will resolve to the user object
+     */
+    fetchCurrentUser: function() {
+      var svc = vm.SessionService;
+      return new Promise(function(resolve, reject) {
+        fetch('/api/sessions')
+          .then(function(resp){
+            return resp.json();
+          }).then(function(json) {
+            if (json.status === "success") {
+              svc.currentUser(json.data);
+              resolve(svc.currentUser());
+            } else {
+              reject(new Error("API call failed", json.message));
+            }
+        }).catch(function(err) {
+          reject(err);
+        });
 
-			});
-		}
+      });
+    }
 
-	},
+  },
 
-	// Services
-	vm.ServicesService = {
-		services: ko.observableArray([]),
+  // Services
+  vm.ServicesService = {
+    services: ko.observableArray([]),
 
-		/**
-		 * Get services for given user
-		 *
-		 * @param {string} userEmail - The username for which to retrieve services
-		 */
-		getServices: function(userEmail) {
-			var svc = vm.ServicesService;
+    /**
+     * Get services for given user
+     *
+     * @param {string} userEmail - The username for which to retrieve services
+     */
+    getServices: function(userEmail) {
+      var svc = vm.ServicesService;
 
-			// Get the user
-			var getUserFn;
-			if (_.isUndefined(userEmail)) {
-				getUserFn = vm.SessionService.getCurrentUser();
-			} else {
-				new Promise(function(resolve, reject) {resolve(userEmail);});
-			}
+      // Get the user
+      var getUserFn;
+      if (_.isUndefined(userEmail)) {
+        getUserFn = vm.SessionService.getCurrentUser();
+      } else {
+        new Promise(function(resolve, reject) {resolve(userEmail);});
+      }
 
-			getUserFn
-				.then(svc.fetchServicesForUser)
-				.then(function(services) {
-					svc.services(services);
-				});
-		},
+      getUserFn
+        .then(svc.fetchServicesForUser)
+        .then(function(services) {
+          svc.services(services);
+        });
+    },
 
-		/**
-		 * Fetch user's services and update observables
-		 *
-		 * @param {string} user - current user
-		 * @returns A Promise which evaluates to the users' services
-		 */
-		fetchServicesForUser: function(user) {
-			var svc = vm.ServicesService;
-			// Get user's services
-			return new Promise(function(resolve, reject) {
-				fetch('/api/sessions/' + user.email + "/services")
-				.then(function(resp) {
-					return resp.json();
-				}).then(function(json) {
-					if (json.status === "success") {
-						svc.services(json.data);
-						resolve(svc.services());
-					} else {
-						reject(new Error("API call failed", json.message));
-					}
-				}).catch(function(err) {
-					reject(err);
-				});
-			});
-		}
+    /**
+     * Fetch user's services and update observables
+     *
+     * @param {string} user - current user
+     * @returns A Promise which evaluates to the users' services
+     */
+    fetchServicesForUser: function(user) {
+      var svc = vm.ServicesService;
+      // Get user's services
+      return new Promise(function(resolve, reject) {
+        fetch('/api/sessions/' + user.email + "/services")
+        .then(function(resp) {
+          return resp.json();
+        }).then(function(json) {
+          if (json.status === "success") {
+            svc.services(json.data);
+            resolve(svc.services());
+          } else {
+            reject(new Error("API call failed", json.message));
+          }
+        }).catch(function(err) {
+          reject(err);
+        });
+      });
+    }
 
-	},
+  },
 
-	// Controllers
-	vm.ServicesCtrl = {
-		pageSize: ko.observable(10),
-		currentPage: ko.observable(0),
-		numPages: ko.pureComputed(function() {
-			var ctrl = vm.ServicesCtrl;
-			return Math.Ceil(vm.ServicesService.services() / ctrl.pageSize()) + 1;
-		}),
-		pagedServices: ko.pureComputed(function() {
-			var ctrl = vm.ServicesCtrl;
-			var startingIndex = ctrl.currentPage() * ctrl.pageSize();
-			var services = vm.ServicesService.services();
+  // Controllers
+  vm.ServicesCtrl = {
+    pageSize: ko.observable(10),
+    currentPage: ko.observable(0),
+    numPages: ko.pureComputed(function() {
+      var ctrl = vm.ServicesCtrl;
+      return Math.Ceil(vm.ServicesService.services() / ctrl.pageSize()) + 1;
+    }),
+    pagedServices: ko.pureComputed(function() {
+      var ctrl = vm.ServicesCtrl;
+      var startingIndex = ctrl.currentPage() * ctrl.pageSize();
+      var services = vm.ServicesService.services();
 
-			// Return early if not enough data
-			if (services.length == 0 || startingIndex > services.length) { return []; }
+      // Return early if not enough data
+      if (services.length == 0 || startingIndex > services.length) { return []; }
 
-			return vm.ServicesService.services().slice(ctrl.currentPage() * ctrl.pageSize(), ctrl.pageSize());
-		}),
+      return vm.ServicesService.services().slice(ctrl.currentPage() * ctrl.pageSize(), ctrl.pageSize());
+    }),
 
-		/**
-		 * Setup function for the ServicesCtrl (only run once)
-		 */
-		setup: function() {
-			vm.ServicesService.getServices();
-		}
-	};
+    /**
+     * Setup function for the ServicesCtrl (only run once)
+     */
+    setup: function() {
+      vm.ServicesService.getServices();
+    }
+  };
 
-	vm.ManageCtrl = {};
-	vm.ManageUsersCtrl = {users: []};
-	vm.ManageServicesCtrl = {services: []};
-	vm.StatisticsCtrl = {};
+  vm.ManageCtrl = {};
+  vm.ManageUsersCtrl = {users: []};
+  vm.ManageServicesCtrl = {services: []};
+  vm.StatisticsCtrl = {};
 
-	/**
-	 * App initialization function, to be run once, when the app starts
-	 */
-	vm.init = function() {
-		vm.SessionService.fetchCurrentUser();
-	};
+  /**
+   * App initialization function, to be run once, when the app starts
+   */
+  vm.init = function() {
+    vm.SessionService.fetchCurrentUser();
+  };
 
-	vm.init();
+  vm.init();
 }
 
 // Attach VM
--- public/js/routes.js
+++ public/js/routes.js
@@ -1,60 +1,70 @@
 /* global Router */
 /* global location */
-
+/* global _ */
 'use strict';
 
-var AppVM = window.App.VM;
-
 // Routes for App
-AppVM.ServicesRoute = { controllers: ['ServicesCtrl'] };
-AppVM.ManageRoute = { controllers: [] };
-AppVM.ManageUsersRoute = { controllers: [] };
-AppVM.ManageServicesRoute = { controllers: [] };
-AppVM.StatisticsRoute = { controllers: [] };
-
-/**
- * Generate pre-route utility function
- * The pre-route utility function runs before the users provided route function, and does some seutp/cleanup tasks
- *
- * @param {string} route - The path of the route
- */
-function generatePreRouteHelper(route) {
-  if (!_.isString(route)) { throw new Error("Only string routes are allowed, invalid route: [" + route + "]"); }
-  return function() {
-    // Go to a route when it is navigated to
-    AppVM.gotoRoute(route);
-  };
-}
-
-
-// Route registration
-window.App.Routes = {
-  default: '/services',
-  '/services': AppVM.ServicesRoute,
-  '/manage': AppVM.ManageRoute,
-  '/manage/users': AppVM.ManageUsersRoute,
-  '/manage/services': AppVM.ManageServicesRoute,
-  '/statistics': AppVM.StatisticsRoute
-};
-
-// Add PreRouteHelper to all routes
-_.forEach(window.App.Routes, function(v, k) {
-  if (k !== "default") {
-    // Create an internal list of functions to call when the route happens
-    window.App.Routes[k]._fns = [generatePreRouteHelper(k), window.App.Routes[k].fn || null];
-  }
+RegisterRoute('ServicesRoute', {
+  controllers: ['ServicesCtrl'],
+  url: '/services'
+});
+RegisterRoute('ManageRoute', {
+  controllers: [],
+  url: '/manage'
+});
+RegisterRoute('ManageUsersRoute', {
+  controllers: [],
+  url: '/manage/users'
+});
+RegisterRoute('ManageServicesRoute', {
+  controllers: [],
+  url: '/manage/services'
+});
+RegisterRoute('StatisticsRoute', {
+  controllers: [],
+  url: '/statistics'
 });
 
 /**
  * Create and initialize Director's router with the internal list of functions for each route
  */
 window.App.Router = Router(
-  _.zipObject(_.keys(window.App.Routes),
+  _.zipObject(_.map(window.App.Routes, 'url'),
               _.map(window.App.Routes, '_fns'))
 );
 window.App.Router.init();
 
-// Go to default route
-if (location.hash === '' && _.has(window.App.Routes, 'default')) {
-  AppVM.gotoRoute(window.App.Routes.default);
+// Go to default route on initial page load
+if (location.hash === ''){ window.App.VM.gotoRoute(window.App.VM.ServicesRoute); }
+
+
+/**
+ * Generate pre-route utility function
+ * The pre-route utility function runs before the users provided route function, and does some seutp/cleanup tasks
+ *
+ * @param {object} route - The route object being modified
+ */
+function generatePreRouteHelper(route) {
+  if (!_.isObject(route)) {
+    throw new Error("Only string routeUrls are allowed, invalid route object: [" + route + "]");
+  }
+
+  return function() {
+    // Go to a routeUrl when it is navigated to
+    window.App.VM.gotoRoute(route);
+  };
+}
+
+/**
+ * Utility function for registering routes
+ *
+ * @param {string} - Name of the route
+ * @param {object} - The route object
+ */
+function RegisterRoute(name, route) {
+  if (!_.isString(name)) { throw new Error("Expect name of registered route to be a string"); }
+  if (!_.isObject(route)) { throw new Error("Expect route to be an object"); }
+
+  route._fns = [generatePreRouteHelper(route), route.fn || null];
+  window.App.Routes[route.url] = window.App.VM[name] = route;
 }
