t3hmrman/casgo

commit 5d7fb95f4f0cdbe3c8424ba67f0259a2295da9ea

Add api keys table setup/teardown methods to adapter, stub out api tests, refactor authentication in API code

cas/rethinkdb_adapter.go
@@ -12,6 +12,7 @@ func (db *RethinkDBAdapter) GetDbName() string            { return db.dbName }
 func (db *RethinkDBAdapter) GetTicketsTableName() string  { return db.ticketsTableName }
 func (db *RethinkDBAdapter) GetServicesTableName() string { return db.servicesTableName }
 func (db *RethinkDBAdapter) GetUsersTableName() string    { return db.usersTableName }
+func (db *RethinkDBAdapter) GetAPIKeysTableName() string  { return db.apiKeysTableName }
 
 func NewRethinkDBAdapter(c *CAS) (*RethinkDBAdapter, error) {
 	// Database setup
@@ -25,12 +26,17 @@ func NewRethinkDBAdapter(c *CAS) (*RethinkDBAdapter, error) {
 
 	// Create the adapter
 	adapter := &RethinkDBAdapter{
-		session:           dbSession,
-		dbName:            c.Config["dbName"],
-		ticketsTableName:  "tickets",
-		servicesTableName: "services",
-		usersTableName:    "users",
-		LogLevel:          c.Config["logLevel"],
+		session:              dbSession,
+		dbName:               c.Config["dbName"],
+		ticketsTableName:     "tickets",
+		ticketsTableOptions:  nil,
+		servicesTableName:    "services",
+		servicesTableOptions: &r.TableCreateOpts{PrimaryKey: "name"},
+		usersTableName:       "users",
+		usersTableOptions:    &r.TableCreateOpts{PrimaryKey: "email"},
+		apiKeysTableName:     "api_keys",
+		apiKeysTableOptions:  &r.TableCreateOpts{PrimaryKey: "key"},
+		LogLevel:             c.Config["logLevel"],
 	}
 
 	return adapter, nil
@@ -76,6 +82,7 @@ func (db *RethinkDBAdapter) Setup() *CASServerError {
 	db.SetupServicesTable()
 	db.SetupTicketsTable()
 	db.SetupUsersTable()
+	db.SetupApiKeysTable()
 
 	return nil
 }
@@ -134,7 +141,7 @@ func (db *RethinkDBAdapter) createTableWithOptions(tableName string, dbOptions i
 
 // Set up the table that holds services
 func (db *RethinkDBAdapter) SetupServicesTable() *CASServerError {
-	return db.setupTable(db.servicesTableName, r.TableCreateOpts{PrimaryKey: "name"})
+	return db.setupTable(db.servicesTableName, db.servicesTableOptions)
 }
 
 // Tear down the table that holds services
@@ -144,7 +151,7 @@ func (db *RethinkDBAdapter) TeardownServicesTable() *CASServerError {
 
 // Set up the table that holds tickets
 func (db *RethinkDBAdapter) SetupTicketsTable() *CASServerError {
-	return db.setupTable(db.ticketsTableName, nil)
+	return db.setupTable(db.ticketsTableName, db.ticketsTableOptions)
 }
 
 // Tear down the table that holds tickets
@@ -154,12 +161,22 @@ func (db *RethinkDBAdapter) TeardownTicketsTable() *CASServerError {
 
 // Set up the table that holds users
 func (db *RethinkDBAdapter) SetupUsersTable() *CASServerError {
-	return db.setupTable(db.usersTableName, r.TableCreateOpts{PrimaryKey: "email"})
+	return db.setupTable(db.usersTableName, db.usersTableOptions)
 }
 
 // Tear down the table that holds users
 func (db *RethinkDBAdapter) TeardownUsersTable() *CASServerError {
 	return db.teardownTable(db.usersTableName)
+}
+
+// Set up the table that holds apikeys
+func (db *RethinkDBAdapter) SetupApiKeysTable() *CASServerError {
+	return db.setupTable(db.apiKeysTableName, db.apiKeysTableOptions)
+}
+
+// Tear down the table that holds apikeys
+func (db *RethinkDBAdapter) TeardownApiKeysTable() *CASServerError {
+	return db.teardownTable(db.apiKeysTableName)
 }
 
 // Dynamically setup tables - dispatch because each table might have special implementations
@@ -171,6 +188,8 @@ func (db *RethinkDBAdapter) SetupTable(tableName string) *CASServerError {
 		return db.SetupServicesTable()
 	case db.usersTableName:
 		return db.SetupUsersTable()
+	case db.apiKeysTableName:
+		return db.SetupApiKeysTable()
 	default:
 		casError := &FailedToSetupDatabaseError
 		return casError
@@ -201,6 +220,8 @@ func (db *RethinkDBAdapter) getTableSetupOptions(tableName string) (*r.TableCrea
 		return db.servicesTableOptions, nil
 	case db.usersTableName:
 		return db.usersTableOptions, nil
+	case db.apiKeysTableName:
+		return db.apiKeysTableOptions, nil
 	default:
 		return nil, errors.New(fmt.Sprintf("Invalid tableName, can't find setup options for table [%s]", tableName))
 	}
@@ -215,6 +236,8 @@ func (db *RethinkDBAdapter) setTableSetupOptions(tableName string, opts *r.Table
 		db.servicesTableOptions = opts
 	case db.usersTableName:
 		db.usersTableOptions = opts
+	case db.apiKeysTableName:
+		db.apiKeysTableOptions = opts
 	default:
 		return errors.New(fmt.Sprintf("Failed to set table setup options for table [%s]", tableName))
 	}

cas/api.go
@@ -2,7 +2,6 @@ package cas
 
 import (
 	"github.com/gorilla/mux"
-	"github.com/gorilla/sessions"
 	"net/http"
 	"strings"
 )
@@ -15,14 +14,33 @@ func NewCasgoFrontendAPI(c *CAS) (*FrontendAPI, error) {
 	return &FrontendAPI{casServer: c}, nil
 }
 
-// Utility function to retrieve session and user information
-func getSessionAndUser(api *FrontendAPI, req *http.Request) (*sessions.Session, *User, *CASServerError) {
+// Utility function to authenticate an API user, whether user is using a web-session or passed an API key
+func authenticateAPIUser(api *FrontendAPI, req *http.Request) (*User, *CASServerError) {
+
+	// Attempt to authenticate with HTTP session
+	user, casErr := authenticateWithSession(api, req)
+	if casErr == nil {
+		return user, nil
+	}
+
+	// Attempt to authenticate with API key and secret if present
+	user, casErr = api.authenticateWithAPIKey(req)
+	if casErr == nil {
+		return user, nil
+	}
+
+	// If all authentication methods fail, return error
+	return nil, &FailedToAuthenticateUserError
+}
+
+// Authenticate user with session
+func authenticateWithSession(api *FrontendAPI, req *http.Request) (*User, *CASServerError) {
 	// Get the current session
 	session, err := api.casServer.cookieStore.Get(req, "casgo-session")
 	if err != nil {
 		casErr := &FailedToRetrieveServicesError
 		casErr.err = &err
-		return nil, nil, casErr
+		return nil, casErr
 	}
 
 	// Retrive current user from session
@@ -30,10 +48,22 @@ func getSessionAndUser(api *FrontendAPI, req *http.Request) (*sessions.Session, 
 	if !ok {
 		casErr := &FailedToRetrieveInformationFromSessionError
 		casErr.err = &err
-		return nil, nil, casErr
+		return nil, casErr
 	}
 
-	return session, &user, nil
+	return &user, nil
+}
+
+func (api *FrontendAPI) authenticateWithAPIKey(req *http.Request) (*User, *CASServerError) {
+	// Get the api key and secret
+	apiKey := req.Header.Get("X-Api-Key")
+	apiSecret := req.Header.Get("X-Api-Secret")
+	if len(apiKey) == 0 || len(apiSecret) == 0 {
+		return nil, &FailedToAuthenticateUserError
+	}
+
+	// Query the database?
+	return nil, nil
 }
 
 // Hook up API endpoints to given mux
@@ -51,7 +81,7 @@ func (api *FrontendAPI) HookupAPIEndpoints(m *mux.Router) {
 
 // Handle sessions endpoint
 func (api *FrontendAPI) SessionsHandler(w http.ResponseWriter, req *http.Request) {
-	_, user, casErr := getSessionAndUser(api, req)
+	user, casErr := authenticateAPIUser(api, req)
 	if casErr != nil {
 		api.casServer.render.JSON(w, casErr.httpCode, map[string]string{
 			"status":  "error",
@@ -68,7 +98,7 @@ func (api *FrontendAPI) SessionsHandler(w http.ResponseWriter, req *http.Request
 
 // Get the services for a logged in user
 func (api *FrontendAPI) listSessionUserServices(w http.ResponseWriter, req *http.Request) {
-	_, user, casErr := getSessionAndUser(api, req)
+	user, casErr := authenticateAPIUser(api, req)
 	if casErr != nil {
 		api.casServer.render.JSON(w, casErr.httpCode, map[string]string{
 			"status":  "error",
@@ -97,11 +127,10 @@ func (api *FrontendAPI) listSessionUserServices(w http.ResponseWriter, req *http
 	})
 }
 
-
 // Get list of services (admin only)
 func (api *FrontendAPI) GetServices(w http.ResponseWriter, req *http.Request) {
 	// Get the current session and user
-	_, user, casErr := getSessionAndUser(api, req)
+	user, casErr := authenticateAPIUser(api, req)
 	if casErr != nil {
 		api.casServer.render.JSON(w, casErr.httpCode, map[string]string{
 			"status":  "error",
@@ -138,7 +167,7 @@ func (api *FrontendAPI) GetServices(w http.ResponseWriter, req *http.Request) {
 // Create a new service
 func (api *FrontendAPI) CreateService(w http.ResponseWriter, req *http.Request) {
 	// Get session and user
-	_, user, casErr := getSessionAndUser(api, req)
+	user, casErr := authenticateAPIUser(api, req)
 	if casErr != nil {
 		api.casServer.render.JSON(w, casErr.httpCode, map[string]string{
 			"status":  "error",
@@ -183,7 +212,7 @@ func (api *FrontendAPI) CreateService(w http.ResponseWriter, req *http.Request) 
 // Returns the removed service's name
 func (api *FrontendAPI) RemoveService(w http.ResponseWriter, req *http.Request) {
 	// Get session and user
-	_, user, casErr := getSessionAndUser(api, req)
+	user, casErr := authenticateAPIUser(api, req)
 	if casErr != nil {
 		api.casServer.render.JSON(w, casErr.httpCode, map[string]string{
 			"status":  "error",
@@ -223,7 +252,7 @@ func (api *FrontendAPI) RemoveService(w http.ResponseWriter, req *http.Request) 
 // Returns the modified service
 func (api *FrontendAPI) UpdateService(w http.ResponseWriter, req *http.Request) {
 	// Get session and user
-	_, user, casErr := getSessionAndUser(api, req)
+	user, casErr := authenticateAPIUser(api, req)
 	if casErr != nil {
 		api.casServer.render.JSON(w, casErr.httpCode, map[string]string{
 			"status":  "error",

cas/api_test/api_suite_test.go
@@ -6,12 +6,15 @@ import (
 
 	"github.com/t3hmrman/casgo/cas"
 	"testing"
+	"net/http/httptest"
 )
 
+// Testing globals for HTTP tests
+var testHTTPServer *httptest.Server
 var testCASConfig map[string]string
 var testCASServer *cas.CAS
 
-func TestCas(t *testing.T) {
+func TestCasgoAPI(t *testing.T) {
 	RegisterFailHandler(Fail)
 	RunSpecs(t, "CasGo API Suite")
 }
@@ -25,6 +28,9 @@ var _ = BeforeSuite(func() {
 	})
 	testCASServer, _ = cas.NewCASServer(testCASConfig)
 	testCASServer.SetupDb()
+
+	// Setup http test server
+	testHTTPServer = httptest.NewServer(testCASServer.ServeMux)
 
 	// Load database fixtures
 	testCASServer.Db.LoadJSONFixture(
@@ -41,5 +47,6 @@ var _ = BeforeSuite(func() {
 })
 
 var _ = AfterSuite(func() {
+	testHTTPServer.Close()
 	testCASServer.TeardownDb()
 })

cas/api_test/api_test.go
@@ -1,52 +1,74 @@
 package api_test
 
 import (
+	"encoding/json"
 	. "github.com/onsi/ginkgo"
 	. "github.com/onsi/gomega"
-	. "github.com/t3hmrman/casgo/cas"
+	"io/ioutil"
+	"log"
+	"net/http"
 )
 
 var _ = Describe("CasGo API", func() {
 
-	Describe("#HookupAPIEndpoints", func() {
-		It("Should hookup an endpoint for listing services (GET /api/services)", func() {})
-		It("Should hookup an endpoint for creating services (POST /api/services)", func() {})
-		It("Should hookup an endpoint for updating services (PUT /api/services/{servicename})", func() {})
-		It("Should hookup an endpoint for deleting services (DELETE /api/services/{servicename})", func() {})
-		It("Should hookup an endpoint for retrieving services for a user (GET /api/sessions/{userEmail}/services)", func() {})
-		It("Should hookup an endpoint for retrieving logged in users's session (GET /api/sessions)", func() {})
+	Describe("#authenticateAPIUser", func() {
+		It("Should fail for unauthenticated users", func() {
+			resp, err := http.Get(testHTTPServer.URL + "/api/services")
+			Expect(err).To(BeNil())
+
+			rawBody, err := ioutil.ReadAll(resp.Body)
+			Expect(err).To(BeNil())
+
+			var respJSON map[string]interface{}
+			json.Unmarshal(rawBody, &respJSON)
+			Expect(respJSON["status"]).To(Equal("error"))
+
+			log.Println("Response: %v", respJSON)
+		})
+		// It("Should authenticate a user without a session who has passed an API key", func() {
+		//	http.Get(testHTTPServer.URL + "/api/services")
+		// })
 	})
 
-	Describe("Session handler", func() {
-		It("Should return an error if there is no session", func() {})
-	})
+	// Describe("#HookupAPIEndpoints", func() {
+	//	It("Should hookup an endpoint for listing services (GET /api/services)", func() {})
+	//	It("Should hookup an endpoint for creating services (POST /api/services)", func() {})
+	//	It("Should hookup an endpoint for updating services (PUT /api/services/{servicename})", func() {})
+	//	It("Should hookup an endpoint for deleting services (DELETE /api/services/{servicename})", func() {})
+	//	It("Should hookup an endpoint for retrieving services for a user (GET /api/sessions/{userEmail}/services)", func() {})
+	//	It("Should hookup an endpoint for retrieving logged in users's session (GET /api/sessions)", func() {})
+	// })
 
-	Describe("Current user's services listing endpoint", func() {
-		It("Should return an error if there is no user logged in", func() {})
-	})
+	// Describe("Session handler", func() {
+	//	It("Should return an error if there is no session", func() {})
+	// })
 
-	Describe("#getSessionAndUser", func() {
-		It("SHould fail and return an error if there is no session", func() {})
-	})
+	// Describe("Current user's services listing endpoint", func() {
+	//	It("Should return an error if there is no user logged in", func() {})
+	// })
 
-	Describe("#GetServices (GET /services)", func() {
-		It("Should list all services for an admin user", func() {})
-		It("Should display an error for non-admin users", func() {})
-	})
+	// Describe("#getSessionAndUser", func() {
+	//	It("SHould fail and return an error if there is no session", func() {})
+	// })
 
-	Describe("#CreateService (POST /services)", func() {
-		It("Should create a service for an admin user", func() {})
-		It("Should display an error for non-admin users", func() {})
-	})
+	// Describe("#GetServices (GET /services)", func() {
+	//	It("Should list all services for an admin user", func() {})
+	//	It("Should display an error for non-admin users", func() {})
+	// })
 
-	Describe("#RemoveService (DELETE /services)", func() {
-		It("Should create a service for an admin user", func() {})
-		It("Should display an error for non-admin users", func() {})
-	})
+	// Describe("#CreateService (POST /services)", func() {
+	//	It("Should create a service for an admin user", func() {})
+	//	It("Should display an error for non-admin users", func() {})
+	// })
 
-	Describe("#UpdateService (PUT /services)", func() {
-		It("Should create a service for an admin user", func() {})
-		It("Should display an error for non-admin users", func() {})
-	})
+	// Describe("#RemoveService (DELETE /services)", func() {
+	//	It("Should create a service for an admin user", func() {})
+	//	It("Should display an error for non-admin users", func() {})
+	// })
+
+	// Describe("#UpdateService (PUT /services)", func() {
+	//	It("Should create a service for an admin user", func() {})
+	//	It("Should display an error for non-admin users", func() {})
+	// })
 
 })

cas/errors.go
@@ -58,7 +58,12 @@ var (
 	InvalidServiceNameError = CASServerError{
 		msg:        "Invalid service name provided.",
 		httpCode:   http.StatusBadRequest,
-		casErrCode: 107,
+		casErrCode: 108,
+	}
+	FailedToAuthenticateUserError = CASServerError{
+		msg:        "Failed to authenticate API user. Please ensure that you have provided sufficient credentials (whether through relevant headers or session information)..",
+		httpCode:   http.StatusUnauthorized,
+		casErrCode: 109,
 	}
 
 	// Internal Server errors (error codes 200 - 299)

cas/types.go
@@ -143,6 +143,8 @@ type RethinkDBAdapter struct {
 	servicesTableOptions *r.TableCreateOpts
 	usersTableName       string
 	usersTableOptions    *r.TableCreateOpts
+	apiKeysTableName     string
+	apiKeysTableOptions  *r.TableCreateOpts
 	LogLevel             string
 }
 

commit 07e0fb2661c1fe15fe2146caf0e89cf7de6d615a

Fix table creation bug that was causing tests to fail (likely introduced by commit 5d7fb95)

cas/rethinkdb_adapter.go
@@ -98,15 +98,10 @@ func (db *RethinkDBAdapter) teardownTable(tableName string) *CASServerError {
 }
 
 func (db *RethinkDBAdapter) setupTable(tableName string, dbOptions interface{}) *CASServerError {
-	// Catch nil options
-	if dbOptions == nil {
-		return db.createTableWithOptions(tableName, nil)
-	}
-
-	// Ensure dbOptions is of correct type, if non-nil
+	// Ensure rdbOptions is of correct type, if non-nil
 	switch t := dbOptions.(type) {
-	case r.TableCreateOpts:
-		return db.createTableWithOptions(tableName, dbOptions)
+	case *r.TableCreateOpts:
+		return db.createTableWithOptions(tableName, dbOptions.(*r.TableCreateOpts))
 	default:
 		casError := &FailedToSetupTableError
 		err := fmt.Errorf("Unexpected type of dbOptions: %T", t)
@@ -116,26 +111,37 @@ func (db *RethinkDBAdapter) setupTable(tableName string, dbOptions interface{}) 
 	return nil
 }
 
-func (db *RethinkDBAdapter) createTableWithOptions(tableName string, dbOptions interface{}) *CASServerError {
-	logMessagef(db.LogLevel, "INFO", "Creating table [%s], options: %v", tableName, dbOptions)
+func (db *RethinkDBAdapter) createTableWithOptions(tableName string, rdbOptions *r.TableCreateOpts) *CASServerError {
+	logMessagef(db.LogLevel, "INFO", "Creating table [%s], options: %v", tableName, rdbOptions)
 
-	// Check again that dbOptions is not nil, optionally leave out argument
+	// Check again that rdbOptions is not nil, optionally leave out argument
 	var err error
-	if dbOptions == nil {
+	if rdbOptions == nil {
+		
+		// Create table with no options
 		_, err = r.Db(db.dbName).TableCreate(tableName).Run(db.session)
-	} else {
-		// Cast the options to the type we expect them to be
-		castedOptions := dbOptions.(r.TableCreateOpts)
 
-		// Perform query & save the options for later use
-		_, err = r.Db(db.dbName).TableCreate(tableName, castedOptions).Run(db.session)
-		db.setTableSetupOptions(tableName, &castedOptions)
+	} else {
+
+		// Set and get the table options (so that they can be retrieved later
+		db.setTableSetupOptions(tableName, rdbOptions)
+		options, err := db.getTableSetupOptions(tableName)
+		if err != nil {
+			casError := &FailedToCreateTableError
+			casError.err = &err
+			return casError
+		}
+
+		// Create table
+		_, err = r.Db(db.dbName).TableCreate(tableName, *options).Run(db.session)
 	}
+
 	if err != nil {
 		casError := &FailedToCreateTableError
 		casError.err = &err
 		return casError
 	}
+
 	return nil
 }
 