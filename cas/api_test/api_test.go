e01620bdc3e07c73193b85f2e1ff30d1743a5f03
t3hmrman <t3hmrman@gmail.com>
2015-01-04 01:42:02 -0600

    Add tests for deleting services

--- cas/api_test/api_test.go
+++ cas/api_test/api_test.go
@@ -14,15 +14,16 @@ import (
 )
 
 var API_TEST_DATA map[string]string = map[string]string{
-	"exampleAdminOnlyURI":   "/api/services",
-	"exampleRegularUserURI": "/api/sessions",
-	"userApiKey":            "userapikey",
-	"userApiSecret":         "badsecret",
-	"adminApiKey":           "adminapikey",
-	"adminApiSecret":        "badsecret",
-	"fakeServiceName":       "test_service_2",
-	"fakeServiceUrl":        "localhost:3001/validateCASLogin",
-	"fakeServiceAdminEmail": "admin@test.com",
+	"exampleAdminOnlyURI":          "/api/services",
+	"exampleRegularUserURI":        "/api/sessions",
+	"userApiKey":                   "userapikey",
+	"userApiSecret":                "badsecret",
+	"adminApiKey":                  "adminapikey",
+	"adminApiSecret":               "badsecret",
+	"fakeServiceName":              "test_service_3",
+	"fakeServiceUrl":               "localhost:3002/validateCASLogin",
+	"fakeServiceAdminEmail":        "admin@test.com",
+	"nameOfFixtureServiceToDelete": "test_service_2",
 }
 
 // List of tuples that describe all endpoints hierarchically
@@ -41,6 +42,52 @@ var EXPECTED_API_ENDPOINTS map[string][]StringTuple = map[string][]StringTuple{
 	},
 }
 
+// Helper that checks to ensure unauthorized error response from performing an API request
+func expectInsufficientPermissionsFromAPIRequest(req *http.Request) {
+	// Perform request
+	_, _, respJSON := jsonAPIRequestWithCustomHeaders(req)
+	Expect(respJSON).NotTo(BeNil())
+	Expect(respJSON["status"]).To(Equal("error"))
+	Expect(respJSON["message"]).To(Equal(InsufficientPermissionsError.Msg))
+}
+
+// Helper function to create new fake service
+func createNewFakeService(service CASService) map[string]interface{} {
+	// Craft request with admin user's API key
+	req, err := http.NewRequest(
+		"POST",
+		testHTTPServer.URL+"/api/services",
+		strings.NewReader(
+			url.Values{
+				"name":       {service.Name},
+				"url":        {service.Url},
+				"adminEmail": {service.AdminEmail},
+			}.Encode()),
+	)
+	Expect(err).To(BeNil())
+
+	// Set header for request
+	req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+	req.Header.Add("X-Api-Key", API_TEST_DATA["adminApiKey"])
+	req.Header.Add("X-Api-Secret", API_TEST_DATA["adminApiSecret"])
+
+	// Perform request
+	_, _, respJSON := jsonAPIRequestWithCustomHeaders(req)
+	Expect(respJSON).NotTo(BeNil())
+	Expect(respJSON["status"]).To(Equal("success"))
+
+	// Get the list of services that was returned (dependent on fixture)
+	var newService map[string]interface{}
+	Expect(respJSON["data"]).To(BeAssignableToTypeOf(newService))
+	newService = respJSON["data"].(map[string]interface{})
+	Expect(newService["name"]).To(Equal(API_TEST_DATA["fakeServiceName"]))
+	Expect(newService["url"]).To(Equal(API_TEST_DATA["fakeServiceUrl"]))
+	Expect(newService["adminEmail"]).To(Equal(API_TEST_DATA["fakeServiceAdminEmail"]))
+
+	return newService
+}
+
+// Function to be used with client creation to disallow redirects from API
 func failRedirect(req *http.Request, via []*http.Request) error {
 	Expect(req).To(BeNil())
 	return errors.New("No redirects allowed")
@@ -114,9 +161,7 @@ var _ = Describe("CasGo API", func() {
 			req.Header.Add("X-Api-Secret", API_TEST_DATA["userApiSecret"])
 
 			// Perform JSON API request
-			_, _, respJSON := jsonAPIRequestWithCustomHeaders(req)
-			Expect(respJSON["status"]).To(Equal("error"))
-			Expect(respJSON["message"]).To(Equal(InsufficientPermissionsError.Msg))
+			expectInsufficientPermissionsFromAPIRequest(req)
 		})
 
 	})
@@ -152,19 +197,26 @@ var _ = Describe("CasGo API", func() {
 			_, _, respJSON := jsonAPIRequestWithCustomHeaders(req)
 			Expect(respJSON["status"]).To(Equal("success"))
 
-			// Get the list of services that was returned (dependent on fixture)
+			// Get the list of services that was returned
 			var rawServicesList []interface{}
 			Expect(respJSON["data"]).To(BeAssignableToTypeOf(rawServicesList))
 			rawServicesList = respJSON["data"].([]interface{})
-			Expect(len(rawServicesList)).To(Equal(1))
+			Expect(len(rawServicesList)).To(BeNumerically(">", 0))
 
-			// Check the map that represents the service
+			// Check the list of services for map that represents a service we expect to exist
+			foundExpectedService := false
 			var serviceMap map[string]interface{}
-			Expect(rawServicesList[0]).To(BeAssignableToTypeOf(serviceMap))
-			serviceMap = rawServicesList[0].(map[string]interface{})
-			Expect(serviceMap["name"]).To(Equal("test_service"))
-			Expect(serviceMap["url"]).To(Equal("localhost:3000/validateCASLogin"))
-			Expect(serviceMap["adminEmail"]).To(Equal("admin@test.com"))
+			for _, rawMap := range rawServicesList {
+				Expect(rawMap).To(BeAssignableToTypeOf(serviceMap))
+				serviceMap = rawMap.(map[string]interface{})
+				if name, ok := serviceMap["name"]; ok && name == "test_service" {
+					foundExpectedService = true
+					Expect(serviceMap["name"]).To(Equal("test_service"))
+					Expect(serviceMap["url"]).To(Equal("localhost:3000/validateCASLogin"))
+					Expect(serviceMap["adminEmail"]).To(Equal("admin@test.com"))
+				}
+			}
+			Expect(foundExpectedService).To(BeTrue())
 		})
 	})
 
@@ -189,8 +241,8 @@ var _ = Describe("CasGo API", func() {
 			Expect(respJSON["message"]).To(Equal(InvalidServiceError.Msg))
 		})
 
-		It("Should successfully create a service given valid input from an admin user", func() {
-			// Craft request with admin user's API key
+		It("Should display an error for non-admin users", func() {
+			// Craft request with regular user's API key
 			req, err := http.NewRequest(
 				"POST",
 				testHTTPServer.URL+"/api/services",
@@ -205,6 +257,33 @@ var _ = Describe("CasGo API", func() {
 
 			// Set header for request
 			req.Header.Set("Content-Type", "application/x-www-form-urlencoded")
+			req.Header.Add("X-Api-Key", API_TEST_DATA["userApiKey"])
+			req.Header.Add("X-Api-Secret", API_TEST_DATA["userApiSecret"])
+
+			// Perform request
+			expectInsufficientPermissionsFromAPIRequest(req)
+		})
+
+		It("Should successfully create a service given valid input from an admin user", func() {
+			createNewFakeService(CASService{
+				Name:       API_TEST_DATA["fakeServiceName"],
+				Url:        API_TEST_DATA["fakeServiceUrl"],
+				AdminEmail: API_TEST_DATA["fakeServiceAdminEmail"],
+			})
+		})
+	})
+
+	Describe("#RemoveService (DELETE /services)", func() {
+		It("Should properly delete a service for an admin user", func() {
+			// Craft request with admin user's API key
+			req, err := http.NewRequest(
+				"DELETE",
+				testHTTPServer.URL+"/api/services/"+API_TEST_DATA["nameOfFixtureServiceToDelete"],
+				nil,
+			)
+			Expect(err).To(BeNil())
+
+			// Set header for request
 			req.Header.Add("X-Api-Key", API_TEST_DATA["adminApiKey"])
 			req.Header.Add("X-Api-Secret", API_TEST_DATA["adminApiSecret"])
 
@@ -212,23 +291,26 @@ var _ = Describe("CasGo API", func() {
 			_, _, respJSON := jsonAPIRequestWithCustomHeaders(req)
 			Expect(respJSON).NotTo(BeNil())
 			Expect(respJSON["status"]).To(Equal("success"))
-
-			// Get the list of services that was returned (dependent on fixture)
-			var newService map[string]interface{}
-			Expect(respJSON["data"]).To(BeAssignableToTypeOf(newService))
-			newService = respJSON["data"].(map[string]interface{})
-			Expect(newService["name"]).To(Equal(API_TEST_DATA["fakeServiceName"]))
-			Expect(newService["url"]).To(Equal(API_TEST_DATA["fakeServiceUrl"]))
-			Expect(newService["adminEmail"]).To(Equal(API_TEST_DATA["fakeServiceAdminEmail"]))
+			Expect(respJSON["data"]).To(Equal(API_TEST_DATA["nameOfFixtureServiceToDelete"]))
 		})
 
-		//It("Should display an error for non-admin users", func() {})
-	})
+		It("Should display an error for non-admin users", func() {
+			// Craft request with regular user's API key
+			req, err := http.NewRequest(
+				"DELETE",
+				testHTTPServer.URL+"/api/services/"+API_TEST_DATA["fakeServiceName"],
+				nil,
+			)
+			Expect(err).To(BeNil())
 
-	// Describe("#RemoveService (DELETE /services)", func() {
-	//	It("Should create a service for an admin user", func() {})
-	//	It("Should display an error for non-admin users", func() {})
-	// })
+			// Set header for request
+			req.Header.Add("X-Api-Key", API_TEST_DATA["userApiKey"])
+			req.Header.Add("X-Api-Secret", API_TEST_DATA["userApiSecret"])
+
+			// Perform request
+			expectInsufficientPermissionsFromAPIRequest(req)
+		})
+	})
 
 	// Describe("#UpdateService (PUT /services)", func() {
 	//	It("Should create a service for an admin user", func() {})
